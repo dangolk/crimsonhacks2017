bf80e7be640c3080a6950a97db5062ede904d1f6
Joseph Sommer <jjs451@msstate.edu>
2017-03-25 16:18:38 -0500

    Initial commit

---
+++ server/eb-flask/application.py
@@ -0,0 +1,11 @@
+from flask import Flask 
+application = Flask(__name__)
+
+@application.route('/')
+def hello_world():
+	return "Hello world!"
+
+
+if __name__ == '__main__':
+	application.debug = True
+	application.run()
