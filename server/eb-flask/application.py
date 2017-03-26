003647acba392b3a38c246cc3244d54afce44346
Joseph Sommer <jjs451@msstate.edu>
2017-03-25 19:20:55 -0500

    Added /parse

--- server/eb-flask/application.py
+++ server/eb-flask/application.py
@@ -1,10 +1,14 @@
-from flask import Flask 
+from flask import Flask, request, json
 application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-	return "Hello world!"
+	return "Hello world!\n I updated the file"
 
+@application.route('/parse', methods=['POST'])
+def parse_text():
+	content = request.get_json()
+	return content["text"]
 
 if __name__ == '__main__':
 	application.debug = True
